c31ed7d95559aa4691ae8358a84b68ea82cccca2
Mozafari1 <m_rahmat@hotmail.no>
2020-09-03 17:18:56 +0200

    IP

---
+++ ip_repo/selection_sort_IP_1.0/src/reg.vhd
@@ -0,0 +1,26 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY reg IS
+	PORT (
+		clk, rst, load : IN STD_LOGIC;
+		input : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+		output : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+
+	);
+END;
+
+ARCHITECTURE arch OF reg IS
+
+BEGIN
+	-- clk process
+	PROCESS (clk, rst)
+	BEGIN
+		IF (rst = '1') THEN
+			output <= (OTHERS => '0');
+		ELSIF rising_edge(clk) AND load = '1' THEN
+			output <= input;
+		END IF;
+	END PROCESS;
+
+END;
